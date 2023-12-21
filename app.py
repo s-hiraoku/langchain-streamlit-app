099ee88c44ac5c842f3b62b96c82c2c35a56d906
hiraoku.shinichi <s.hiraoku@gmail.com>
2023-12-21 10:25:24 +0900

    Respond to output responses that include conversation history.

--- app.py
+++ app.py
@@ -4,10 +4,12 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import load_tools, initialize_agent, AgentType
+from langchain.memory import ConversationBufferMemory
+from langchain.prompts import MessagesPlaceholder
 from langchain.callbacks import StreamlitCallbackHandler
 
 
-def create_def_chain():
+def create_agent_chain():
     chat = ChatOpenAI(
         model_name=os.environ["OPENAI_API_MODEL"],
         temperature=os.environ["OPENAI_API_TEMPERATURE"],
@@ -15,8 +17,19 @@ def create_def_chain():
         streaming=True,
     )
 
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")]
+    }
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+
     tools = load_tools(["ddg-search", "wikipedia"])
-    return initialize_agent(tools, chat, agent=AgentType.OPENAI_FUNCTIONS)
+    return initialize_agent(
+        tools,
+        chat,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
 
 
 load_dotenv()
@@ -31,6 +44,9 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+if "agent_chain" not in st.session_state:
+    st.session_state.agent_chain = create_agent_chain()
+
 prompt = st.chat_input("What is up?")
 
 
@@ -42,8 +58,7 @@ if prompt:
 
     with st.chat_message("assistant"):
         callback = StreamlitCallbackHandler(st.container())
-        agent_chain = create_def_chain()
-        response = agent_chain.run(prompt, callbacks=[callback])
+        response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
         st.markdown(response)
 
     st.session_state.messages.append({"role": "assistant", "content": response})
